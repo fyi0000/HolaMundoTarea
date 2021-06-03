7e7d72b48bbd834ed0e5632e54446438f332a4b4
fyi0000 <44799726+fyi0000@users.noreply.github.com>
2021-06-03 19:13:25 +0200

    Update descargaModelo.py

--- src/descargaModelo.py
+++ src/descargaModelo.py
@@ -1,5 +1,6 @@
 import gdown
 
-url="https://drive.google.com/file/d/1Pt8Q7kT1ytPk9gBrHBpQBOlfKS1NEUmK/view?usp=sharing"
+url="https://drive.google.com/uc?id=1Pt8Q7kT1ytPk9gBrHBpQBOlfKS1NEUmK"
+
 
 gdown.download(url, output='./modelo-0.1.pth', quiet=True)
